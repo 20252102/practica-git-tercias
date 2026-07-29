e2fcd6b4416c76e714d2f04ae6743ccc4a852b8a
20252102 <2025310522@uteq.edu.mx>
2026-07-29 16:59:04 -0600

    Calculado el maximo y minimo para el estudiante C

--- matematicas.py
+++ matematicas.py
@@ -2,3 +2,7 @@
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 
 print("Resultados del analisis estadistico:")
+# Desarrollado por Estudiante C
+maximo = max(numeros)
+minimo = min(numeros)
+print(f"Maximo: {maximo} | Minimo: {minimo}")
